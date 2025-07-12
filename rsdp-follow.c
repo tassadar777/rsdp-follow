fb85bb664bc40e905b0938d42d5ff27e17b3487b
Andrew Ivanchuk <nnk592@gmail.com>
2025-07-12 15:43:24 +0300

    follow the reference in FADT to fetch DSDT table

--- rsdp-follow.c
+++ rsdp-follow.c
@@ -58,6 +58,85 @@ struct ACPISDTHeader {
   uint32_t CreatorRevision;
 };
 
+struct GenericAddressStructure_s
+{
+  uint8_t AddressSpace;
+  uint8_t BitWidth;
+  uint8_t BitOffset;
+  uint8_t AccessSize;
+  uint64_t Address;
+};
+
+typedef struct GenericAddressStructure_s GenericAddressStructure;
+
+struct FADT
+{
+    struct   ACPISDTHeader h;
+    uint32_t FirmwareCtrl;
+    uint32_t Dsdt;
+
+    // field used in ACPI 1.0; no longer in use, for compatibility only
+    uint8_t  Reserved;
+
+    uint8_t  PreferredPowerManagementProfile;
+    uint16_t SCI_Interrupt;
+    uint32_t SMI_CommandPort;
+    uint8_t  AcpiEnable;
+    uint8_t  AcpiDisable;
+    uint8_t  S4BIOS_REQ;
+    uint8_t  PSTATE_Control;
+    uint32_t PM1aEventBlock;
+    uint32_t PM1bEventBlock;
+    uint32_t PM1aControlBlock;
+    uint32_t PM1bControlBlock;
+    uint32_t PM2ControlBlock;
+    uint32_t PMTimerBlock;
+    uint32_t GPE0Block;
+    uint32_t GPE1Block;
+    uint8_t  PM1EventLength;
+    uint8_t  PM1ControlLength;
+    uint8_t  PM2ControlLength;
+    uint8_t  PMTimerLength;
+    uint8_t  GPE0Length;
+    uint8_t  GPE1Length;
+    uint8_t  GPE1Base;
+    uint8_t  CStateControl;
+    uint16_t WorstC2Latency;
+    uint16_t WorstC3Latency;
+    uint16_t FlushSize;
+    uint16_t FlushStride;
+    uint8_t  DutyOffset;
+    uint8_t  DutyWidth;
+    uint8_t  DayAlarm;
+    uint8_t  MonthAlarm;
+    uint8_t  Century;
+
+    // reserved in ACPI 1.0; used since ACPI 2.0+
+    uint16_t BootArchitectureFlags;
+
+    uint8_t  Reserved2;
+    uint32_t Flags;
+
+    // 12 byte structure; see below for details
+    GenericAddressStructure ResetReg;
+
+    uint8_t  ResetValue;
+    uint8_t  Reserved3[3];
+
+    // 64bit pointers - Available on ACPI 2.0+
+    uint64_t                X_FirmwareControl;
+    uint64_t                X_Dsdt;
+
+    GenericAddressStructure X_PM1aEventBlock;
+    GenericAddressStructure X_PM1bEventBlock;
+    GenericAddressStructure X_PM1aControlBlock;
+    GenericAddressStructure X_PM1bControlBlock;
+    GenericAddressStructure X_PM2ControlBlock;
+    GenericAddressStructure X_PMTimerBlock;
+    GenericAddressStructure X_GPE0Block;
+    GenericAddressStructure X_GPE1Block;
+};
+
 int doChecksumXsdp(struct XSDP_t *tableHeader)
 {
     unsigned char sum = 0;
@@ -129,6 +208,17 @@ void dump_table(int fd,off_t offset)
 		goto error;
 	}
 	close(outfd);
+
+	//follow FADT DSDT pointer and try to fetch DSDT
+	if(!strncmp(name,"FACP",4)){
+		struct FADT *fp;
+		uint64_t dsdtp;
+
+		printf("FASP found, trying to fetch DSDT...\n");
+		fp=(struct FADT*) p;
+		dsdtp= (uint64_t) fp->Dsdt;
+		dump_table(fd,dsdtp);
+	}
 
 	if(p!=NULL)
 		free(p);
